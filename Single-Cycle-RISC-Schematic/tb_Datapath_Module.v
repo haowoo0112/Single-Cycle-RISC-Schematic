12aa12afa0d974bc48febc5da323889f5053db96
hao0112 <haowoo0112@gmail.com>
2022-10-19 20:12:10 +0800

    feat: Datapath_Module testbench- JAL1

--- Single-Cycle-RISC-Schematic/tb_Datapath_Module.v
+++ Single-Cycle-RISC-Schematic/tb_Datapath_Module.v
@@ -107,11 +107,33 @@ module Datapath_Module_Datapath_Module_sch_tb();
 		//test_ADDI();
 		//test_SUBI();
 		//test_BRANCH();
-		test_JMP();
+		//test_JMP();
+		test_JAL1();
 		
 		#30;
 		$finish;
 	end
+
+	task test_JAL1;
+	begin
+		write_instr_mem(16'h0, 16'b00011_001_000_00000);//LDR
+		write_instr_mem(16'h1, 16'b10001_010_00000111);//JAL1
+		write_instr_mem(16'h8, 16'b11100_000_010_000_00);//Out
+		write_data_mem(16'h0, 16'h1234);
+		#20;
+		ext_instr_we = 1'b0;
+		test_normal = 1'b0;
+		clr = 1;
+		#10;
+		clr = 0;
+		instr_LDR();
+		#10;
+		instr_JAL1();
+		#20;
+		instr_OutR();
+		#20;
+	end
+	endtask
 
 	task test_JMP;
 	begin
@@ -738,6 +760,36 @@ module Datapath_Module_Datapath_Module_sch_tb();
 		LHI = 0;
 		LLI = 0;
 		RF_write_en = 0;
+		flag_OutR = 0;
+	end
+	endtask
+
+	task instr_JAL1;
+	begin
+		flag_HLT = 1'b1;
+		
+		test_normal = 1'b0;
+		data_write_en = 0;
+		
+		flag_label_PC = 0;
+		flag_Rm_PC = 0;
+		flag_Rd_PC = 0;
+		BRANCH = 1;
+		
+		ADC = 0;
+		SUB = 0;
+		SBB = 0;
+		JMP = 0;
+		Src_ALU_B = 0;
+		Src_Read_B = 0;
+		
+		flag_mem_RF = 0;
+		flag_ALU_RF = 0;
+		flag_Rm_RF = 0;
+		flag_PC_RF = 1;
+		LHI = 0;
+		LLI = 0;
+		RF_write_en = 1;
 		flag_OutR = 0;
 	end
 	endtask
